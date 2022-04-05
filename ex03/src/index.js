f5efaee762147718882b8017ca637849c34932ff
Vedran V <drvicko@hotmail.com>
2022-04-05 11:34:33 +0200

    ex03 Equality and Strict Equality operator

---
+++ ex03/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function myEqualFunction (num) {
+    if (num === 23) {
+        return "Equal";
+    }
+    return "Not equal"; 
+}
+
+myEqualFunction(); 
+
+
+
+// Only change code above this line
+
+module.exports = myEqualFunction; 
