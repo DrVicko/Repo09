48bc27824e240494cbd48ba5e4789f32cae45fda
Vedran V <drvicko@hotmail.com>
2022-04-05 14:52:04 +0200

    ex15 Switch statement

---
+++ ex15/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+function switchCase (letter) {
+    switch (letter) {
+        case "a" : return "antelope";
+        break;
+        case "b" : return "bird";
+        break; 
+        case "c" : return "cat";
+        break;
+        default : return "stuff";
+        break; 
+    }
+}
+switchCase(); 
+
+// Only change code above this line
+
+module.exports = switchCase;
