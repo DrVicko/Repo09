0e3a36deea7029ca91fd00275c2fcda09632bcea
Vedran V <drvicko@hotmail.com>
2022-04-05 14:40:26 +0200

    ex14 Ternary operator

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function checkSign (num) {
+    return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero";
+}
+
+checkSign(); 
+
+// Only change code above this line
+
+module.exports = checkSign;
