53fe73ed169b96ddf89f7792850efd7b47b0fa0c
Vedran V <drvicko@hotmail.com>
2022-04-05 11:24:20 +0200

    ex01 Boolean

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function myTrueBoolean () {
+    var myTrue = true; 
+    return myTrue
+}
+
+myTrueBoolean(); 
+
+// Only change code above this line
+
+module.exports = myTrueBoolean; 
