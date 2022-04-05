ec25f8b4f625994f787f1f2c5a9097bfb937db0b
Vedran V <drvicko@hotmail.com>
2022-04-05 11:49:26 +0200

    ex05 Inequality operator

---
+++ ex05/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function compareStrictValues (m) {
+    if (m != 55) {
+        return "Not equal";
+    }
+    return "Equal";
+}
+
+compareStrictValues(); 
+
+
+
+// Only change code above this line
+
+module.exports = compareStrictValues;
