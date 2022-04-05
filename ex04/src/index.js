68ed46dec8901bfd7e255a6ac6e7636c0cd5088f
Vedran V <drvicko@hotmail.com>
2022-04-05 11:42:12 +0200

    ex04 Comparing different values

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function compareDifferentValues (m, n) {
+    if (typeof m === typeof n) {
+        return "Equal";
+    }
+    return "Not equal"; 
+}
+
+compareDifferentValues(5, "5"); 
+
+
+
+// Only change code above this line
+
+module.exports = compareDifferentValues; 
