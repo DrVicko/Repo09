3578b00be71118540bb70b513db0f45e0e51db59
Vedran V <drvicko@hotmail.com>
2022-04-05 13:47:30 +0200

    ex08 Logical And Operator - &&

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function logicalAndOperator (num) {
+    if (num >= 40 && num <= 80) {
+        return "Yes";
+    }
+    return "No";
+}
+
+logicalAndOperator(); 
+
+
+// Only change code above this line
+
+module.exports = logicalAndOperator;
